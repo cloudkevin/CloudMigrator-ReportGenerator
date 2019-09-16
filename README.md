127b5e54b30c52ff144e16b96ae6e2c51fab563e
Kevin Tuuri <cloudkevin@users.noreply.github.com>
2019-09-15 22:58:22 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CloudMigrator-ReportGenerator
---
+++ cmReportGenerator.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+import os, zipfile, csv, glob
+import pandas as pd
+from pathlib import Path
+from progress.bar import Bar
+
+extension = '.zip'
+header = ['UserId','State','Status','TotalImportSuccess','TotalImportFailure','TotalExportSuccess','TotalExportFailure','EmailExportSuccess','EmailExportFailure','AppointmentExportSuccess','AppointmentExportFailure','TaskExportSuccess','TaskExportFailure','ContactExportSuccess','ContactExportFailure','GroupExportSuccess','GroupExportFailure','GroupMemberExportSuccess','GroupMemberExportFailure','DocumentExportSuccess','DocumentExportFailure','OtherExportSuccess','OtherExportFailure','FolderExportFailure','EmailImportSuccess','EmailImportFailure','AppointmentImportSuccess','AppointmentImportFailure','TaskImportSuccess','TaskImportFailure','ContactImportSuccess','ContactImportFailure','GroupImportSuccess','GroupImportFailure','GroupMemberImportSuccess','GroupMemberImportFailure','DocumentImportSuccess','DocumentImportFailure','OtherImportSuccess','OtherImportFailure','StartTime','EndTime','Duration','SizeImported','ServerId','BLANK','CSV File Path']
+summaryHeader = ['UserId','TotalErrors','ErrorPercentage','TotalImportFailure','TotalExportFailure']
+logFiles = []
+importFailures = []
+exportFailures = []
+
+def unzipArchive(dir_name,extension): # unzip all migration reports in directory
+    for item in os.listdir(dir_name): # loop through items in dir
+        if item.endswith(extension): # check for ".zip" extension
+            file_name = os.path.abspath(item) # get full path of files
+            stripped_name = os.path.splitext(item)[0] # strip file extension for new folder name
+            zip_ref = zipfile.ZipFile(file_name) # create zipfile object
+            new_path = os.path.join(dir_name, stripped_name) # create path that points to the folder that we will create
+            try:
+                os.mkdir(stripped_name) # create folder removing .zip from name
+                zip_ref.extractall(new_path) # extract file to dir
+                os.remove(file_name) # delete zipped file
+            except OSError as error:
+                print(error)
+            zip_ref.close() # close file
+    for filename in Path(dir_name).glob('**/UserStatistics.csv'):
+        logFiles.append(str(filename)) # create list of all extracted UserStatistics.csv files
+    for imfail in Path(dir_name).glob('**/ItemResultImport-*.csv'):
+        importFailures.append(str(imfail))
+    for exfail in Path(dir_name).glob('**/ItemResultExport-*.csv'):
+        exportFailures.append(str(exfail))
+    totalBar.next()
+
+
+def rawReport():
+    lineCount = 0
+    for file in logFiles:
+        if file.endswith('.csv'):
+            with open(file,'r') as f1:
+                csv_reader = csv.reader(f1, delimiter=',')
+                with open('RawReport.csv','a', newline='') as f2:
+                    csv_writer = csv.writer(f2,delimiter=',')
+                    if lineCount == 0:
+                        csv_writer.writerow(header)
+                        lineCount += 1
+                    next(csv_reader,None)
+                    for row in csv_reader:
+                        data = row + [file[:-4]]
+                        csv_writer.writerow(data)
+    totalBar.next()
+
+def importFailureReport():
+    global imErrorCount
+    lineCount = 0
+    for file in importFailures:
+        if file.endswith('.csv'):
+            with open(file,'r') as f1:
+                csv_reader = csv.reader(f1, delimiter=',')
+                with open('ImportFailureReport.csv','a', newline='') as f2:
+                    csv_writer = csv.writer(f2,delimiter=',')
+                    if lineCount == 0:
+                        csv_writer.writerow(['DetailedType','ResponseCode','GUID','Reason','UserId','Failure','BLANK','CSV File Path'])
+                        lineCount += 1
+                    next(csv_reader,None)
+                    for row in csv_reader:
+                        data = row + [file[:-4]]
+                        csv_writer.writerow(data)
+    df = pd.read_csv('ImportFailureReport.csv')
+    df2 = df.sort_values(by=['UserId', 'Failure'])
+    df2.drop_duplicates(subset="Failure", keep = 'first', inplace = True)
+    imErrorCount = df2.groupby(['UserId']).count()
+    imErrorCount = imErrorCount.drop(['ResponseCode','GUID','Reason','Failure','BLANK','CSV File Path'], axis=1)
+    imErrorCount = imErrorCount.rename(columns={"UserId": "UserId", "DetailedType": "ImportErrorCount"})
+    totalBar.next()
+
+def exportFailureReport():
+    global exErrorCount
+    lineCount = 0
+    for file in exportFailures:
+        if file.endswith('.csv'):
+            with open(file,'r') as f1:
+                csv_reader = csv.reader(f1, delimiter=',')
+                with open('ExportFailureReport.csv','a', newline='') as f2:
+                    csv_writer = csv.writer(f2,delimiter=',')
+                    if lineCount == 0:
+                        csv_writer.writerow(['DetailedType','ResponseCode','GUID','Reason','UserId','Failure','BLANK','CSV File Path'])
+                        lineCount += 1
+                    next(csv_reader,None)
+                    for row in csv_reader:
+                        data = row + [file[:-4]]
+                        csv_writer.writerow(data)
+    df = pd.read_csv('ExportFailureReport.csv')
+    df = (df[df['Reason'] != 'The remote server returned an error: (500) Internal Server Error.']).sort_values(by=['UserId', 'Failure']) # exclude specific errors from dataframe
+    exErrorCount = df.groupby(['UserId']).count() # count number of errors by UserId
+    exErrorCount = exErrorCount.drop(['ResponseCode','GUID','Reason','Failure','BLANK','CSV File Path'], axis=1) # drop columns unneeded columns
+    exErrorCount = exErrorCount.rename(columns={"UserId": "UserId", "DetailedType": "ExportErrorCount"}) # rename columns to allow proper join
+    totalBar.next()
+
+
+def combineDuplicates(): # combine duplicates using UserId, add all numerical values and drop the rest
+    df = pd.read_csv('RawReport.csv')
+    df2 = df.groupby(['UserId']).max()
+    df2.to_csv('CombinedReport.csv',header=True)
+    totalBar.next()
+
+
+def generateSummary():
+    summary = imErrorCount.join(exErrorCount, on='UserId')
+    summary = summary.fillna(0)
+    summary['TotalErrorCount'] = summary['ImportErrorCount'] + summary['ExportErrorCount']
+    cr = pd.read_csv('CombinedReport.csv',index_col=['UserId'])
+    cr = cr.join(summary, on='UserId')
+    cr['Error Percentage'] = cr['ImportErrorCount']/cr['TotalImportSuccess']
+    cr = cr.filter(['UserId','ImportErrorCount','ExportErrorCount','TotalErrorCount','TotalImportSuccess','Error Percentage','SizeImported'])
+    cr.to_csv('MigrationSummary.csv',header=True)
+    totalBar.next()
+
+
+def mergeToExcel(dir_name,client_name):
+    generatedReports = []
+    writer = pd.ExcelWriter(client_name+'_MigrationReport.xlsx', engine='xlsxwriter')
+    for item in os.listdir(dir_name):
+        if item.endswith('.csv'):
+            generatedReports.append(item)
+    for r in generatedReports:
+        df = pd.read_csv(r)
+        stripped_name = os.path.splitext(r)[0]
+        df.to_excel(writer, sheet_name=stripped_name,index=False)
+    writer.save()
+    os.remove('MigrationSummary.csv')
+    os.remove('ImportFailureReport.csv')
+    os.remove('ExportFailureReport.csv')
+    totalBar.next()
+    totalBar.finish()
+    print("\n\n######################################")
+    print("**All Reports Generated Successfully**")
+    print("######################################")
+    print("\nFinal Report Location: "+ os.path.abspath(writer)+'\n')
+
+
+def main():
+    global totalBar
+    print('\n')
+    print('####################################################################################################')
+    print(r'   _____ __  __   _____                       _      _____                           _             ')
+    print(r'  / ____|  \/  | |  __ \                     | |    / ____|                         | |            ')
+    print(r' | |    | \  / | | |__) |___ _ __   ___  _ __| |_  | |  __  ___ _ __   ___ _ __ __ _| |_ ___  _ __ ')
+    print(r" | |    | |\/| | |  _  // _ \ '_ \ / _ \| '__| __| | | |_ |/ _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__|")
+    print(r' | |____| |  | | | | \ \  __/ |_) | (_) | |  | |_  | |__| |  __/ | | |  __/ | | (_| | || (_) | |   ')
+    print(r'  \_____|_|  |_| |_|  \_\___| .__/ \___/|_|   \__|  \_____|\___|_| |_|\___|_|  \__,_|\__\___/|_|   ')
+    print(r'                            | |                                                                    ')
+    print(r'                            |_|                                                                    ')
+    print('####################################################################################################')
+    print('\n')
+    dir_name = input("Log File Directory Path: ")
+    client_name = input("Enter client prefix: ")
+    print('\n')
+    os.chdir(dir_name) # change directory from working dir to dir with files
+    previousReport = client_name + '_MigrationReport.xlsx'
+    if os.path.exists(previousReport): # do not run if previous report already generated
+        print("\nERROR: Report has already been generated in this directory.\n")
+    else:
+        totalBar = Bar('Processing',max=7,fill='$')
+        print('\n')
+        unzipArchive(dir_name,extension)
+        rawReport()
+        importFailureReport()
+        exportFailureReport()
+        combineDuplicates()
+        generateSummary()
+        mergeToExcel(dir_name,client_name)
+
+if __name__ == '__main__':
+    main()
